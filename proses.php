5718c1c11f94a17fce500fa03873e7bb0855eda5
Angelina Yona Listianingsih <43510088+angelinayl@users.noreply.github.com>
2018-09-28 10:09:15 +0700

    Form Proses
    Ini merupakan form proses yang terdiri dari hobi, send, delete, browse file, dan upload.

--- proses.php
+++ proses.php
@@ -1,4 +1,28 @@
-<?php
+<form>
+<tr> 
+	<h2><b><center> <td> Hobi	: </td> </center> </b> </h2>
+<td> <center> <input type="checkbox" name="Hobi[]" value="Memasak"> Memasak </center></td>
+<br><br>
+<td> <center> <input type="checkbox" name="Hobi[]" value="Menyanyi"> Menyanyi <center></td>
+<br><br>
+<td> <center> <input type="checkbox" name="Hobi[]" value="Menari"> Menari </center></td>
+<br><br>
+</tr>
+
+</form>
+<center> <input type="submit" name="Send" value="Send"> <input type="submit" name="Delete" value="Delete"></center>
+<br>
+<center> <input type="file" name="dokumen" required></center>
+<br>
+<center> <input type="submit" name="Upload" value="Upload"></center>
 
 
-?>
+<?php 
+//if(isset($_POST['Send'])){
+//	$data = $_POST["Hobi"]
+//	foreach($data as $nilai)
+
+//} echo $nilai 
+ //echo "<br>";
+
+ ?>
